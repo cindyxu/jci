78ff7d4121f1572447f592d77b2e884cff23b436
Cindy Xu <shuc.xu@gmail.com>
2017-01-21 00:28:01 -0500

    Added panda test

---
+++ saddest-panda/Assets/Hand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+
+	public float minAngle;
+	public float maxAngle;
+	public float speed;
+
+	private float mCurrFrac;
+
+	// Use this for initialization
+	void Start () {
+		mCurrFrac = Random.value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mCurrFrac = (mCurrFrac + Time.deltaTime * speed) % 1.0f;
+		float angle = minAngle + Mathf.Abs (mCurrFrac - 0.5f) * 2 * (maxAngle - minAngle);
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+	}
+	
+}
---
+++ saddest-panda/Assets/HandGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGenerator : MonoBehaviour {
+
+	public int numHands;
+
+	// Use this for initialization
+	void Start () {
+		Collider2D collider = GetComponent<Collider2D> ();
+		Bounds bounds = collider.bounds;
+		Random random = new Random();
+		for (int i = 0; i < numHands; i++) {
+			float x = Random.Range (bounds.min.x, bounds.max.x);
+			float y = Random.Range (bounds.min.y, bounds.max.y);
+			GameObject.Instantiate (Resources.Load ("hand"), new Vector2 (x, y), Quaternion.identity);
+		}
+	}
+}
